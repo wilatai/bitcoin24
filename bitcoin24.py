66881a13b2e42a98461404f9e75c9c59794a2f34
Wilatai <https://github.com/wilatai>
2013-04-01 15:42:53 +0200

    some refactoring and extra error handling

--- bitcoin24.py
+++ bitcoin24.py
@@ -26,73 +26,93 @@ class Bitcoin24:
 		self.API_USER = api_user
 		self.API_KEY  = api_key
 
-	##########################################################################
-	#	Methods that do NOT require an API user + key
-	##########################################################################
+	#########################################################################
+	#	Support methods
+	#########################################################################
 
-	def ticker(self, currency='EUR'):
-		"""Returns the current ticker in either 'EUR' or 'USD'"""
-		resp = requests.get(url=TICKER_URL % currency)
-		if resp.status_code == 200:
-			return resp.json()
-		return resp
-
-	def orderbook(self, currency='EUR'):
-		"""Returns the current order book in either 'EUR' or 'USD'"""
-		resp = requests.get(url=ORDERBOOK_URL % currency)
-		if resp.status_code == 200:
+	def get_request(self, url):
+		"""
+		Makes a simple GET request to the URL provided. Returns a JSON result
+		on HTTP status code 200. In other cases it will return either the 
+		response object or, in case of an exception, None 
+		"""
+		try:
+			resp = requests.get(url)
+			if resp.status_code == 200:
 				return resp.json()
-		return resp
+			return resp			
+		except requests.RequestException as e:
+			print e.message
+			return None
 
-	##########################################################################
-	#	Methods requiring an API user + key
-	##########################################################################
-
-	def request(self, api_method, arg_params={}):
-		"""Makes an API request using the provided parameters"""
+	def post_request(self, api_method, arg_params={}):
+		"""
+		Makes an API POST request using the provided parameters. Returns a
+		JSON result on HTTP status code 200. In other cases it will return
+		either the response object or, in case of an exception, None.
+		"""
 
 		params = {'user': self.API_USER,
 				  'key':  self.API_KEY,
 				  'api':  api_method}
 		params.update(arg_params)
 
-		resp = requests.post(url=API_URL, data=params)
+		try:
+			resp = requests.post(url=API_URL, data=params)
+			if resp.status_code == 200:
+				return resp.json()
+			return resp
+		except requests.RequestException as e:
+			print e.message
+			return None
 
-		if resp.status_code == 200:
-			return resp.json()
-		return resp
+	##########################################################################
+	#	Methods that do NOT require an API user + key
+	##########################################################################
+
+	def ticker(self, currency='EUR'):
+		"""Returns the current ticker in either 'EUR' or 'USD'"""
+		return self.get_request(url=TICKER_URL % currency)
+
+	def orderbook(self, currency='EUR'):
+		"""Returns the current order book in either 'EUR' or 'USD'"""
+		return self.get_request(url=ORDERBOOK_URL % currency)
+
+	##########################################################################
+	#	Methods requiring an API user + key
+	##########################################################################
 
 	def account_balance(self):
 		"""Returns the account balance"""
-		return self.request('get_balance')
+		return self.post_request('get_balance')
 
 	def bitcoin_address(self):
 		"""Returns the users Bitcoin address on Bitcoin-24.com"""
-		return self.request('get_addr')
+		return self.post_request('get_addr')
 
 	def open_orders(self):
 		"""Returns open orders"""
-		return self.request('open_orders')
+		return self.post_request('open_orders')
 
 	def cancel_order(self, order_id):
 		"""Cancels an order"""
-		return self.request('cancel_order', {'id': order_id})
+		return self.post_request('cancel_order', {'id': order_id})
 
 	def buy_btc(self, btc_amount, price_per_btc, currency='EUR'):
 		"""Buys btc_amount of Bitcoin for price_per_btc per Bitcoin in the currency specified ('EUR' (default) or 'USD')"""
-		return self.request('buy_btc', {'amount': btc_amount, 'price': price_per_btc, 'cur': currency})
+		return self.post_request('buy_btc', {'amount': btc_amount, 'price': price_per_btc, 'cur': currency})
 
 	def sell_btc(self, btc_amount, price_per_btc, currency='EUR'):
 		"""Sells btc_amount of Bitcoin for price_per_btc per Bitcoin in the currency specified ('EUR' (default) or 'USD')"""
-		return self.request('sell_btc', {'amount': btc_amount, 'price': price_per_btc, 'cur': currency})
+		return self.post_request('sell_btc', {'amount': btc_amount, 'price': price_per_btc, 'cur': currency})
 
 	def withdraw_btc(self, btc_amount, btc_address):
 		"""Withdraws btc_amount of Bitcoin to Bitcoin address btc_address)"""
-		return self.request('withdraw_btc', {'amount': btc_amount, 'address': btc_address})
+		return self.post_request('withdraw_btc', {'amount': btc_amount, 'address': btc_address})
 
 	def trades(self):
 		"""Returns the users trades in JSON format"""
-		return self.request('trades_json')
+		return self.post_request('trades_json')
 
 if __name__ == '__main__':
 
